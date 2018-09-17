48af2ec0c280335ab5ab6c75bbc5a78573c9579e
Louise <t7861231@gmail.com>
2018-09-17 15:30:37 +0800

    final version

--- guess.py
+++ guess.py
@@ -3,12 +3,17 @@
 #猜對，印出"終於猜對了!"
 #猜錯，告訴他比答案大/小
 
+# 進階=>使用者自訂範圍
 import random
-r = random.randint(1, 100)
+start = input('請決定隨機數字範圍開始值:')
+end = input('請決定隨機數字範圍結束值:')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
 count = 0 # 計數
 while True:
 	count += 1 # 簡潔版 快寫法count = count + 1
-	num = input('請猜數字:(請輸入1~100)')  #input為字串
+	num = input('請猜數字:')  #input為字串
 	num = int(num)           #型別轉換
 	if num == r:
 		print('終於猜對了')
